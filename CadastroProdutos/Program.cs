8248631f3eaed08ad5fd07057411562feac8760b
Pedro Otávio <pedrootavio09@yahoo.com.br>
2026-06-17 18:50:12 -0300

    Initial commit

---
+++ CadastroProdutos/Program.cs
@@ -0,0 +1,114 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/test", () => "Teste de API");
+
+var produtos = new List<Produto>()
+{
+    new Produto() { Id = 1, Nome = "Mouse sem fio", Preco = 99.90M, Estoque = 50 },
+    new Produto() { Id = 2, Nome = "Teclado Gamer", Preco = 249.90M, Estoque = 30 }
+};
+
+app.MapGet("/produtos", () =>
+{
+    return produtos;
+});
+
+app.MapGet("/produtos/{id}", (int id) =>
+{
+    var produto = produtos.FirstOrDefault(x => x.Id == id);
+
+    return produto is not null
+    ? Results.Ok(produto)
+    : Results.NotFound($"Produto com o ID {id} não encontrado.");
+});
+
+app.MapPost("/produtos", (Produto novoProduto) =>
+{
+    produtos.Add(novoProduto);
+
+    return Results.Created();
+});
+
+app.MapPut("/produtos/{id}", (int id, Produto produtoAtualizado) =>
+{
+    var produto = produtos.FirstOrDefault(x => x.Id == id);
+
+    if (produto is null)
+    {
+        return Results.NotFound($"Produto com ID {id} não encontrado.");
+    }
+    
+    produto.Nome = produtoAtualizado.Nome;
+    produto.Preco = produtoAtualizado.Preco;
+    produto.Estoque = produtoAtualizado.Estoque;
+
+    return Results.Ok(produto);
+});
+
+app.MapDelete("/produtos/{id}", (int id) =>
+{
+    var produto = produtos.FirstOrDefault(x => x.Id == id);
+    
+    if(produto is null)
+    {
+        return Results.NotFound($"Produto com ID {id} não encontrado.");
+    }
+
+    produtos.Remove(produto);
+
+    return Results.NoContent();
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+class Produto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
+    public int Estoque { get; set; }
+
+}
